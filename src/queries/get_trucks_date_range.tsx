28d474c754d41ffd5d0d93a507837027d55cf676
aaely <aaron.ely@hotmail.com>
2024-06-17 19:26:45 -0500

    date range fix

--- src/queries/get_trucks_date_range.tsx
+++ src/queries/get_trucks_date_range.tsx
@@ -1,8 +1,8 @@
 import { api } from "../utils/api"
 
-export const getTrucksByDateRange = async (startDate: any, endDate: any) => {
+export const getTrucksByDateRange = async (date1: any, date2: any) => {
     try {
-        const res = await api.post(`/api/trucks_date_range`, { startDate, endDate })
+        const res = await api.post(`/api/trucks_date_range`, { date1, date2 })
         return res.data
     } catch(error) {
         console.log(error)
